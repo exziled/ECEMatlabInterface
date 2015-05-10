1ba9d8e783e034006f57d1599a2f7e50bcdfd97d
Benjamin Carlson <benjamin.carlson@maine.edu>
2015-05-09 23:30:55 -0700

    added mongodb stupport, start of roster management

---
+++ app/models/student.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var submissionSchema = new Schema({
+	questionID: 	Number,
+	questionTag: 	String,
+	history: 		[String]
+});
+
+var studentSchema = new Schema({
+	studentID: 		Number,
+	ipAddress: 		String,
+	firstName: 		String,
+	lastName: 		String,
+	isActive: 		Boolean,
+	submissions: 	[submissionSchema]
+})
+
+var Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
--- track.js
+++ track.js
@@ -3,130 +3,73 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var swig = require('swig');
 var path = require('path');
-var mysql = require('mysql');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like bellow.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ECEMatlabInterface');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback) {
+	console.log('Mongo Connection Successful');
+});
+
+var Student = require('./app/models/student');
 
 server.listen(3000);
 
 
-var sql_conn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'ece486',
-  password : 'ece486',
-  database : 'ece486'
-});
-
-sql_conn.connect();
-
-// Configure view engine as swig and set view path
-app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 // Serve static content ourselves to avoid needing a network
 app.use(express.static(path.join(__dirname, 'public')));
 
-var opts = {
-	range: ['10.0.1.2/24'],
-	nmap: '/usr/bin/nmap',
-}
-
-var student_data = {};
-var student_ip_map = {};
-
-var client_list = [];
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/views/pages/root.html');
-})
+});
 
+app.get('/roster', function(req, res) {
+	res.sendFile(__dirname + '/views/pages/roster.html');
+});
 
-app.get('/data/students', function(req, res) {
+app.post('/admin/roster/add', function(req, res) {
+	
+	// Get Data from post
+	var newStudent = Student({
+		studentID: 	req.body.studentID,
+		firstName: 	req.body.firstName,
+	});
 
-	sql_conn.query('SELECT users.user_id as user_id, users.student_id as student_id, users.ip_address as ip_addr, CONCAT(map.first_name, " ", map.last_name) as name FROM users LEFT JOIN student_id_map as map on users.student_id = map.student_id ORDER BY map.last_name', function(err, rows, fields) {
-		if (err) throw err;
+	var response = {success: null, message: null};
 
-		rows.forEach(function(val, idx, rows) {
-			student_data[val.user_id] = val;
-			student_ip_map[val.ip_addr] = val.user_id;
-		});
+	// On success, return submitted data to angular
+	newStudent.save(function(err, student) {
+		if (err) {
+			response.success = false;
+		} else {
+			response.success = true;
+			response.message = student;
+		}
 
-		// console.log(student_ip_map);
-
-		res.send(JSON.stringify(student_data));
+		res.send(JSON.stringify(response));
 	});
 });
 
-io.on('connection', function (socket) {
+app.get('/data/students', function(req, res) {
 
-	console.log("Client Connected");
+	Student.find({}, function(err, students){
+		if (err) throw err;
 
-	socket.emit('init');
-
-
-	client_list.push(socket);
-
-	// setInterval(function() {
-	// 	console.log("Scanning");
-
-		
-			
-	// 		// socket.emit('clients', report[0].neighbors);
-	// 	});
-
-	// }, 10000);
-
-  // socket.emit('news', { hello: 'world' });
-
-  // socket.on('my other event', function (data) {
-  //   console.log(data);
-  // });
-
-
+		res.send(JSON.stringify(students));
+	});
 });
 
-
-setInterval(function() {
-	if(!client_list.length)
-	{
-		console.log("No Clients, No nmap");
-	} else {
-
-		// Fire up nmap looking for clientss
-		scanner.nmap('discover', opts, function(err, report) {
-			if (err) console.error(err);
-
-			console.log("Scan Complete");
-
-			var ret = []
-
-			// Iterate over returned neighbors and see if they're people we care about
-			report[0].neighbors.forEach(function(val, idx, rows) {
-				if (student_ip_map[val])
-				{
-					console.log("Host Found");
-
-					ret.push({'user_id': student_ip_map[val], 'status': 'active'});
-				}
-			});
-
-			// Send neighbor intersection to clients
-			for(var i = 0; i < client_list.length; i++)
-			{
-				client_list[i].emit('updates:ip', ret);
-			}
-		});
-	
-	}
-}, 1000);
-
-
-// var server = app.listen(3000, function() {
-// 	var host = server.address().address;
-// 	var port = server.address().port;
-
-// 	console.log('Example app listening at http://%s:%s', host, port);
-// })
- 
 
